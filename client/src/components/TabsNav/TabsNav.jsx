60db16c50feb6b44f8443e13b89ec21ab3ea0ce5
Juanse <jsestrada@uninorte.edu.co>
2019-12-01 23:06:14 -0500

    refactor: changed  material-ui withStyles to SCSS

--- client/src/components/TabsNav/TabsNav.jsx
+++ client/src/components/TabsNav/TabsNav.jsx
@@ -2,22 +2,9 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { Box } from '@material-ui/core';
 import styles from './TabsNav.module.scss';
 
-
-const StyledTabs = withStyles({
-  indicator: {
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
-    '& > div': {
-      maxWidth: 40,
-      width: '100%',
-      backgroundColor: '#635ee7',
-    },
-  },
-// eslint-disable-next-line react/jsx-props-no-spreading
-})((props) => <Tabs {...props} TabIndicatorProps={{ children: <div /> }} />);
 
 const StyledTab = withStyles((theme) => ({
   root: {
@@ -42,12 +29,39 @@ export default function TabsNav() {
   };
 
   return (
-    <div className={styles.wrapper}>
-      <StyledTabs centered value={value} onChange={handleChange} aria-label="styled tabs example">
-        <StyledTab label="All Guests" />
-        <StyledTab label="Attended" />
-        <StyledTab label="Not Attended" />
-      </StyledTabs>
-    </div>
+    <Box className={styles.wrapper}>
+      <Tabs
+        classes={{
+          indicator: styles.tabs,
+        }}
+        centered
+        value={value}
+        onChange={handleChange}
+        aria-label="styled tabs example"
+        TabIndicatorProps={{ children: <div /> }}
+      >
+        <Tab
+          classes={{
+            root: styles.tab,
+          }}
+          disableRipple
+          label="All Guests"
+        />
+        <Tab
+          classes={{
+            root: styles.tab,
+          }}
+          disableRipple
+          label="Attended"
+        />
+        <Tab
+          classes={{
+            root: styles.tab,
+          }}
+          disableRipple
+          label="Not Attended"
+        />
+      </Tabs>
+    </Box>
   );
 }
