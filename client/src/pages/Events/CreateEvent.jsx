5450b60aff4fa26814edf98ee430742d2f75cde5
Jesús Camargo R <acjesus@uninorte.edu.co>
2019-12-03 21:41:03 -0500

    refactor: Add Event Form

--- client/src/pages/Events/CreateEvent.jsx
+++ client/src/pages/Events/CreateEvent.jsx
@@ -1,5 +1,204 @@
 import React, { Component } from 'react';
 import { DateTimePicker} from "@material-ui/pickers";
+import {
+    Button,
+    TextField,
+    FormControlLabel,
+    Checkbox,
+    Link,
+    Grid,
+    Typography,
+    Box,
+    InputAdornment,
+  } from '@material-ui/core';
+  import {
+    Formik, Form, Field,
+  } from 'formik';
+  import * as Yup from 'yup';
+  
+  import LockIcon from '@material-ui/icons/Lock';
+  import EmailIcon from '@material-ui/icons/Email';
+  
+  //import styles from './Form.module.scss';
+  
+  const initialValues = {
+    name: 'LOREM IPSUM',
+  
+  };
+  
+  const validationSchema = () => Yup.object().shape({
+    name: Yup.string()
+      .required('This field is required'),
+  });
+  
+  /*const LoginForm = () => (
+    <Box className={styles.wrapper}>
+      <Typography component="h1">
+          Add Event
+      </Typography>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          setSubmitting(true);
+          console.log(values);
+        }}
+      >
+        {({
+          errors,
+          touched,
+        }) => (
+          <Form
+            className={styles.form}
+            noValidate
+          >
+            <Field
+              type="input"
+              as={TextField}
+              placeholder="Name"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Event Name"
+              name="name"
+              InputProps={{
+                'aria-label': 'Event Name',
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <EmailIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Field
+              as={TextField}
+              placeholder="Password"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              error={touched.password && !!errors.password}
+              helperText={errors.password}
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              InputProps={{
+                'aria-label': 'password',
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <LockIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              className={styles.button}
+            >
+            Login
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#lol" variant="body2">
+                Forgot your password?
+                </Link>
+              </Grid>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  );*/
+
+  const EventForm = () =>(
+    <Box /*{className={styles.wrapper}}*/>
+      <Typography component="h1">
+        New Event
+      </Typography>
+      <Formik
+      initialValues={initialValues}
+      validationSchema = {validationSchema}
+      onSubmit={(values, {setSubmitting}) =>{
+        setSubmitting(true);
+        console.log(values);
+      }}
+    >
+      {({ errors , touched }) =>(
+        <Form /*{className={styles.form}}*/ noValidate>
+          <Field
+            type="input"
+            as={TextField}
+            placeholder="Name"
+            variant="outlined"
+            margin="normal"
+            required
+            id="name"
+            label="Event Name"
+            name="name"
+            InputProps={{
+              'aria-label': 'Event Name',
+              endAdornment: (
+                <InputAdornment position="end">
+                  <EmailIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Field
+            type="input"
+            as={TextField}
+            placeholder="Description"
+            variant="outlined"
+            margin="normal"
+            required
+            id="description"
+            label="Event's description"
+            name="description"
+            InputProps={{
+              'aria-label': "Event's description",
+              endAdornment: (
+                <InputAdornment position="end">
+                  <EmailIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Field
+            type="input"
+            as={DateTimePicker}
+            placeholder="Date"
+            variant="outlined"
+            margin="normal"
+            required
+            id="date"
+            label="Date"
+            name="date"
+            InputProps={{
+              'aria-label': "Event's date",
+            }}
+          />
+          <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              /*className={styles.button}*/
+            >
+            Add Event
+            </Button>
+        </Form>
+      )}
+    </Formik>
+    </Box>
+  );
 
 class CreateEvent extends Component{
     constructor(props){
@@ -49,7 +248,7 @@ class CreateEvent extends Component{
     }
 
     render(){
-        return(
+        /*return(
             <div>
                 <h3>Add New Event</h3>
                 <form onSubmit={this.onSubmit}>
@@ -96,7 +295,8 @@ class CreateEvent extends Component{
                     </div>
                 </form>
             </div>
-        );
+        );*/
+        return EventForm();
     }
 }
 
