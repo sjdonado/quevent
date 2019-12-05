5eeb1bd1fba2059a25ec422c6b05fa5221701189
Juanse <jsestrada@uninorte.edu.co>
2019-12-05 00:23:12 -0500

    feat: xlsx sheets to json

--- client/src/pages/Events/AddGuests/AddGuests.jsx
+++ client/src/pages/Events/AddGuests/AddGuests.jsx
@@ -5,6 +5,7 @@ import {
   Box,
 } from '@material-ui/core';
 import { useDropzone } from 'react-dropzone';
+import XLSX from 'xlsx';
 import SaveOutlinedIcon from '@material-ui/icons/SaveOutlined';
 import PageContainer from '../../../components/PageContainer/PageContainer';
 import ActionButton from '../../../components/ActionButton/ActionButton';
@@ -12,7 +13,24 @@ import styles from './AddGuests.module.scss';
 
 function AddGuests() {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  if (acceptedFiles.length > 0) {
+    const file = acceptedFiles[0];
+    const reader = new FileReader();
+    const rABS = !!reader.readAsBinaryString;
+    reader.onload = (e) => {
+      /* Parse data */
+      const bstr = e.target.result;
+      const wb = XLSX.read(bstr, { type: rABS ? 'binary' : 'array' });
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      /* Convert array of arrays */
+      const data = XLSX.utils.sheet_to_json(ws);
 
+      /* Update state */
+    };
+    if (rABS) reader.readAsBinaryString(file); else reader.readAsArrayBuffer(file);
+  }
   return (
     <PageContainer
       title="Add guests"
