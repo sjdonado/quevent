d45f6d27d66b0b020b479a8f2d9e402d2c67a7f3
Juanse <jsestrada@uninorte.edu.co>
2019-12-05 00:40:14 -0500

    feat: adding array of  attendees

--- client/src/pages/Events/AddGuests/AddGuests.jsx
+++ client/src/pages/Events/AddGuests/AddGuests.jsx
@@ -18,16 +18,11 @@ function AddGuests() {
     const reader = new FileReader();
     const rABS = !!reader.readAsBinaryString;
     reader.onload = (e) => {
-      /* Parse data */
       const bstr = e.target.result;
       const wb = XLSX.read(bstr, { type: rABS ? 'binary' : 'array' });
-      /* Get first worksheet */
       const wsname = wb.SheetNames[0];
       const ws = wb.Sheets[wsname];
-      /* Convert array of arrays */
       const data = XLSX.utils.sheet_to_json(ws);
-
-      /* Update state */
     };
     if (rABS) reader.readAsBinaryString(file); else reader.readAsArrayBuffer(file);
   }
